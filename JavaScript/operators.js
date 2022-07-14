180100f1e78b208726a8c9bd239dc19ffdba4f61
Kamal9807 <107097489+Kamal9807@users.noreply.github.com>
2022-07-14 11:33:44 +0530

    Add files via upload
    add

---
+++ JavaScript/operators.js
@@ -0,0 +1,19 @@
+//operators
+number1 = 5;
+number2 = 10;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(++number1);
+console.log(--number2);
+console.log(number1 + 3);
+console.log(number1 < number2); //relational operator
+console.log(number1 === number2); // equality operator
+console.log(number1 !== number2);
+
+//ternary operator
+let personage = 12;
+let type = personage > 18 ? "Adult Ticket" : "Child Ticket";
+console.log(type);
+//date method
+let hour = Date();
+console.log(hour);
